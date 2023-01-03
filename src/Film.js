45a96ef626409151e98846a0d2a1f9e50169b4bf
Patrik <112979285+patriksinox@users.noreply.github.com>
2023-01-03 14:55:43 +0100

    Update Film.js

--- src/Film.js
+++ src/Film.js
@@ -24,48 +24,50 @@ const Film = () => {
 
   return (
     <>
-      <Container>
-        <Row md="2" sm="2" xs="1" className="sekcia-film">
-          <Col className="text-center">
-            <img src={Poster} alt={Title} />
+           {nacitanie ? (
+        <Spinner></Spinner>
+      ) : (
+        <Container>
+          <Row md="2" sm="2" xs="1" className="sekcia-film">
+            <Col className="text-center">
+              <img src={Poster} alt={Title} />
+            </Col>
+            <Col className="text-left">
+              <h1 className="mt-4 text-center">{Title}</h1>
+              <div className="mt-3">
+                <article>
+                  <div className="obal">Rok vydania:</div> {Released} 🎆
+                </article>
+                <article>
+                  <div className="obal">Režisér:</div> {Director} 👨‍💼
+                </article>
+                <article>
+                  <div className="obal">Žáner:</div> {Genre} 🛣️
+                </article>
+                <article>
+                  <div className="obal">Herci:</div> {Actors} 👱‍♀️👨
+                </article>
+                <article>
+                  <div className="obal">Ocenenia:</div> {Awards} 🏆
+                </article>
+                <article>
+                  <div className="obal">IMDB hodnotenie:</div> {imdbRating} 🏅
+                </article>
+              </div>
+            </Col>
+          </Row>
+          <Col className="text-center mt-4">
+            <h3>Príbeh</h3>
+            <p className="film-pribeh">{Plot}</p>
           </Col>
-          <Col className="text-left">
-            <h1 className="mt-4 text-center">{Title}</h1>
-            <div className="mt-3">
-              <article>
-                <div className="obal">Rok vydania:</div> {Released} 🎆
-              </article>
-              <article>
-                <div className="obal">Režisér:</div> {Director} 👨‍💼
-              </article>
-              <article>
-                <div className="obal">Žáner:</div> {Genre} 🛣️
-              </article>
-              <article>
-                <div className="obal">Herci:</div> {Actors} 👱‍♀️👨
-              </article>
-              <article>
-                <div className="obal">Ocenenia:</div> {Awards} 🏆
-              </article>
-              <article>
-                <div className="obal">IMDB hodnotenie:</div> {imdbRating} 🏅
-              </article>
-            </div>
-          </Col>
-        </Row>
-        <Col className="text-center mt-4">
-          <h3>Príbeh</h3>
-          <p className="film-pribeh">{Plot}</p>
-        </Col>
 
-        <div className="text-center mt-5 mb-5">
-          <Link to="/" className="btn btn-primary">
-            Naspäť na filmy
-          </Link>
-        </div>
-        {nacitanie && <Spinner></Spinner>}
-      </Container>
-
+          <div className="text-center mt-5 mb-5">
+            <Link to="/" className="btn btn-primary">
+              Naspäť na filmy
+            </Link>
+          </div>
+        </Container>
+      )}
       <Footer />
     </>
   );
